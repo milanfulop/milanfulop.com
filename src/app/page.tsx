bb7d5aa447de251aafbd683204e2a41cef90e555
Milan <fulopmilan170@gmail.com>
2025-01-29 15:51:11 +0100

    update: deleted personal story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,50 @@
-import Header from '@/components/Header';
+import { MdEmail } from 'react-icons/md';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import styles from './page.module.css';
-import About from '@/components/tabs/About';
-import Projects from '@/components/tabs/Projects';
-import Socials from '@/components/tabs/Socials';
+import ImageBox from '@/components/boxes/ImageBox';
+
+export const dynamic = 'force-static';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Header />
-      <About />
-      <Projects />
-      <Socials />
+      <div className="center">
+        <h3 className="title">Milan Fulop</h3>
+        <h1 className="description">web developer student</h1>
+      </div>
+      <ImageBox 
+        imageUrl="/childhood-photo.webp"
+        alt="My younger self" 
+        loadingType="eager"
+        marginTop='25px'
+        marginBottom='25px'
+      />
+      <div className={styles.svgContainer}>
+        <a 
+          href="https://github.com/milanfulop" 
+          className={styles.svgLink} 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={36} color="#C5C5C5" />
+        </a>
+        <a 
+          href="https://www.linkedin.com/in/milan-fulop/" 
+          className={styles.svgLink} 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={36} color="#C5C5C5" />
+        </a>
+        <a 
+          href="mailto:fulopmilan170@gmail.com" 
+          className={styles.svgLink} 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <MdEmail size={36} color="#C5C5C5" />
+        </a>
+      </div>
     </div>
   );
 }
-/* 
-      <p className={styles.description}>currently working on <a href='https://github.com/devRoot-workshop' target="_blank" rel="noopener noreferrer">devRoot</a>, and <a href='https://github.com/milanfulop/milanfulop.com' target="_blank" rel="noopener noreferrer">milanfulop.com</a>.</p>
-
-*/
